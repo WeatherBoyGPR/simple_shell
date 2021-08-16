3343ecd39252ffcaf695e7919a66f71f29bdb1da
Gino Rossi <gp.rossijr@gmail.com>
2021-08-16 15:11:00 +0000

    added second utility file util2.c

---
+++ util2.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _puts - will print a string as is
+ * @str: string to print
+ *
+ * Return: number of characters printed
+ */
+int _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]):
+
+	return (i);
+}
