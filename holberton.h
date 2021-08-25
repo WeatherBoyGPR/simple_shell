de241268c1dd5c0641efb9cfafb20bce86f6a79d
Isaac Morales <iso.mora.cor@gmail.com>
2021-08-24 20:11:22 -0500

    path

--- holberton.h
+++ holberton.h
@@ -18,7 +18,8 @@ int shellcore(void);
 int line_read(void);
 char **_strtok(char *imt);
 int com_execute(char **arg);
-
+/*PATH*/
+char **dir_path(char *cmd);
 /*util1 file*/
 char *_strdup(char *str);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
