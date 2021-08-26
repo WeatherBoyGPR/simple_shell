14d49d0733b02a5a2f25c400d0a288ed1d8872ff
Gino Rossi <gp.rossijr@gmail.com>
2021-08-26 00:18:03 +0000

    updated core.c to work with non-interactive mode

--- core.c
+++ core.c
@@ -19,29 +19,29 @@ int shellcore(void)
 	while (cont[0])
 	{
 		_puts(prompt);
-		printf("ATTEMPT #%i\n", i);
+		/*printf("ATTEMPT #%i\n", i);*/
 		/*gets input*/
-		getline(&line, &len, stdin);
-		/*segments input*/
-		/*printf("%s\n", line);*/
-		args = linecut(line);
-		/*func to take care of builtins*/
-		stat = com_execute(args);
-		printf("TEST2\n");
-		i++;
-		if (line != NULL)
+		stat = getline(&line, &len, stdin);
+		if (stat < 0)
 		{
-			printf("free%s\n", line);
 			free(line);
+			cont[0] = 0;
 		}
-
-		if (args != NULL)
+		else
 		{
-			/*printf("free%s\n", *(args));*/
-			free(args);
+			/*segments input*/
+			args = linecut(line);
+			/*func to take care of builtins*/
+			stat = com_execute(args);
+			i++;
+			if (line != NULL)
+				free(line);
+
+			if (args != NULL)
+				free(args);
+			args = NULL;
+			line = NULL;
 		}
-		args = NULL;
-		line = NULL;
 	}
 	return (stat);
 }
@@ -91,7 +91,7 @@ int blt_execute(char **args)
 		{"exit", builtin_exit},
 		{NULL, NULL},
 	};
-printf("TEST\n");
+
 	while (builts[i].builtin != NULL)
 	{
 		if (!(_strcmp(args[0], builts[i].prog)))
@@ -111,27 +111,27 @@ int com_execute(char **arg)
 	int stat = 0;
 
 	stat = blt_execute(arg);
-		if (stat < 0)
-		{
+	if (stat < 0)
+	{
 
-			pcs = fork();
-			if (!pcs)
-			{
-				stat = execve(arg[0], arg, NULL);
-				/*if (stat == -1)
-				  perror("error with execve\n");*/
-				free(arg[0]);
-				free(arg);
-				exit(stat);
-			}
-			/*else if (pcs < 0)
-			  perror("error forking\n");*/
-			else
-			{
-				do {
-					waitpid(pcs, &stat, WUNTRACED);
-				} while (!WIFEXITED(stat) && !WIFSIGNALED(stat));
-			}
+		pcs = fork();
+		if (!pcs)
+		{
+			stat = execve(arg[0], arg, NULL);
+			/*if (stat == -1)
+			  perror("error with execve\n");*/
+			free(arg[0]);
+			free(arg);
+			exit(stat);
 		}
+		/*else if (pcs < 0)
+		  perror("error forking\n");*/
+		else
+		{
+			do {
+				waitpid(pcs, &stat, WUNTRACED);
+			} while (!WIFEXITED(stat) && !WIFSIGNALED(stat));
+		}
+	}
 	return (stat);
 }
